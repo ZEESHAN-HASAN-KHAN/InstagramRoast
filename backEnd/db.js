05ff25e36befd6d87683e625896c5611694cae4f
ZEESHAN HASAN KHAN <zeeshan54khan@gmail.com>
2024-11-17 15:04:46 +0530

    Database added

--- backEnd/db.js
+++ backEnd/db.js
@@ -1,11 +1,49 @@
 const { Client } = require('pg')
 
 const client = new Client({
-    connectionString:"DB string"
+    connectionString:process.env.DB
 })
 
 async function dbConnect()
 {
     await client.connect();
+
     console.log('Database is connected');
+    const result = await client.query(`
+                    CREATE TABLE IF NOT EXISTS profiles (
+                id SERIAL PRIMARY KEY,
+                profile_pic_url TEXT,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                full_name VARCHAR(255),
+                follower INTEGER,
+                following INTEGER,
+                biography TEXT,
+                post INTEGER,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+            );
+
+        `)
 }
+
+
+async function addUser(profilePicUrl, username, fullName, follower, following, biography, post) {
+    try {
+        const result = await client.query(
+            `INSERT INTO profiles 
+                (profile_pic_url, username, full_name, follower, following, biography, post) 
+             VALUES 
+                ($1, $2, $3, $4, $5, $6, $7) 
+             RETURNING *;`,
+            [profilePicUrl, username, fullName, follower, following, biography, post]
+        );
+
+        console.log('User added successfully:', result.rows[0]);
+        return result.rows[0];
+    } catch (error) {
+        console.error('Error adding user:', error);
+        throw error;
+    }
+}
+
+module.exports = { dbConnect, addUser };
--- backEnd/index.js
+++ backEnd/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
+const OpenAI=require('openai')
+const openai = new OpenAI({apiKey:process.env.APIKEY});
 const express = require('express')
 const app = express();
 const PORT = 3000
+const { dbConnect, addUser } =require('./db')
 app.use(express.json());
 
 const options = {
@@ -20,10 +23,31 @@ app.post('/roastMe',async (req, res) => {
 
     try {
         const response = await fetch(url, options);
-	    const result = await response.text();
+      const data = await response.json();
+      const result = data.data;
+      //media count is missing
+      const roastData = {
+        name: result.full_name,
+        userName: result.username,
+        follower: result.follower_count,
+        following: result.following_count,
+        isPrivate: result.is_private,
+        bio: result.biography,
+        post: result.media_count,
+      }
+      const profileUrl=result.profile_pic_url
+
+      await addUser(profileUrl,roastData.userName,roastData.name,roastData.follower,roastData.following,roastData.bio,roastData.post)
+      //adding the profile info to database
+      
+      console.log('Data Added successfully');
+
+    const roast= await test(roastData, result.profile_pic_url)
         return res.status(200).json({
-        data:result
-    })
+        data:roast
+        })
+      //ai response data should also store in another table 
+      //
     }
     catch (e)
     {
@@ -33,31 +57,30 @@ app.post('/roastMe',async (req, res) => {
 
 
 app.listen(PORT,() => {
-    console.log('App is listening on the Port ' + PORT)
+  dbConnect();
+  console.log('App is listening on the Port ' + PORT)
 });
-// const OpenAI=require('openai')
-// const openai = new OpenAI({apiKey:''});
-// async function test() {
+async function test(userData,profileUrl) {
 
-//   const completion = await openai.chat.completions.create({
-//     model: "gpt-4o-mini",
-//     messages: [
-//       { "role": "user", "content": "write a haiku about ai" }
-//     ]
-//   });
-//   console.log(completion);
-// }
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        "role": "user", "content": [
+          {
+            type: "text",
+          "text":"You're a pro comedian—roast the person below based on their profile picture and details given."+JSON.stringify(userData)
+          },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url":  profileUrl
+            },
+          }
+      ] }
+    ]
+  });
+  return JSON.stringify(completion.choices[0].message.content);
+}
 // test();
-
-// console.log(JSON.stringify({
-//   "profile_pic_url": "https://scontent-mxp2-1.cdninstagram.com/v/t51.2885-19/404042911_235414609558714_3765834583032986184_n.jpg?stp=dst-jpg_e0_s150x150&_nc_ht=scontent-mxp2-1.cdninstagram.com&_nc_cat=110&_nc_ohc=DQtCQpK7ib4Q7kNvgHn7dZL&_nc_gid=e7509c2d90614156bfd7028a88afb004&edm=AO4kU9EBAAAA&ccb=7-5&oh=00_AYDEXG5TN4g4NEcCnZUQmS3e5ZsYLb8cmaSoZ25zaL8mxQ&oe=673B4428&_nc_sid=164c1d",
-//   "biography": "Software Engineer 🧑‍💻",
-//   "follower_count": 86,
-//   "following_count": 81,
-//   "is_private": false,
-//   "username": "_arghya.deep_",
-//   "full_name": "Arghyadeep Ghosh",
-//   "media_count": 4
-// }
-// ))
 
